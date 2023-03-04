d38e105984c3a97e5e3c1734f0cec8b709f0793f
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-04 23:12:15 +0530

    Added: Code for ability to add a contact in address book

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,16 +1,34 @@
 package com.addressbook;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class AddressBook {
+public class AddressBook extends ContactPerson {
     public static void main(String[] args) {
-        ArrayList<ContactPerson> contact = new ArrayList<>();
+        AddressBook newPerson = new AddressBook();
+        ArrayList<AddressBook> person = new ArrayList<>();
+        System.out.println("Welcome to Address Book Program");
+        Scanner sc = new Scanner(System.in);
+        //Add person address
+        System.out.println("Enter first name");
+        newPerson.setFirstName(sc.nextLine());
+        System.out.println("Enter last name");
+        newPerson.setLastName(sc.nextLine());
+        System.out.println("Enter address");
+        newPerson.setAddress(sc.nextLine());
+        System.out.println("Enter city");
+        newPerson.setCity(sc.nextLine());
+        System.out.println("Enter state");
+        newPerson.setState(sc.nextLine());
+        System.out.println("Enter email");
+        newPerson.setEmail(sc.nextLine());
+        System.out.println("Enter zip code");
+        newPerson.setZip(sc.nextInt());
+        System.out.println("Enter phone number");
+        newPerson.setPhoneNumber(sc.nextLong());
+        person.add(newPerson);
 
-        contact.add(new ContactPerson("Rahul", "Kumar", "Patna", " Patna", "Bihar", "rahul@123", 813211, 9934691));
-        contact.add(new ContactPerson("Ronit", "Rao", "Patna", "Bgp", "Bihar", "priyesh@123", 813211, 8097658));
-
-        for (ContactPerson contactPerson : contact) {
-            System.out.println(contactPerson);
-        }
+        System.out.println(person);
     }
 }
+
--- src/com/addressbook/ContactPerson.java
+++ src/com/addressbook/ContactPerson.java
@@ -11,17 +11,40 @@ public class ContactPerson {
     long phoneNumber;
 
 
-    public ContactPerson(String firstName, String lastName, String address, String city, String state, String email, int zip, long phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
+    public void setEmail(String email) {
         this.email = email;
-        this.zip = zip;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
 
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
 
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public ContactPerson() {
+        super();
     }
 
     public String toString() {
