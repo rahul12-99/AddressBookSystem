17095afa4d4348c421309e2d9658c7e1aa758d61
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-04 22:58:23 +0530

    Added: Code for ability to create a contact in address book

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,7 +1,16 @@
 package com.addressbook;
 
+import java.util.ArrayList;
+
 public class AddressBook {
     public static void main(String[] args) {
-        System.out.println("Welcome to address book program");
+        ArrayList<ContactPerson> contact = new ArrayList<>();
+
+        contact.add(new ContactPerson("Rahul", "Kumar", "Patna", " Patna", "Bihar", "rahul@123", 813211, 9934691));
+        contact.add(new ContactPerson("Ronit", "Rao", "Patna", "Bgp", "Bihar", "priyesh@123", 813211, 8097658));
+
+        for (ContactPerson contactPerson : contact) {
+            System.out.println(contactPerson);
+        }
     }
 }
--- src/com/addressbook/ContactPerson.java
+++ src/com/addressbook/ContactPerson.java
@@ -1,4 +1,32 @@
 package com.addressbook;
 
 public class ContactPerson {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String email;
+    int zip;
+    long phoneNumber;
+
+
+    public ContactPerson(String firstName, String lastName, String address, String city, String state, String email, int zip, long phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.email = email;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+
+
+    }
+
+    public String toString() {
+        return "Person [name=" + this.firstName + " " + this.lastName + ", Address=" + this.address + ", City="
+                + this.city + ", state=" + this.state + ", email=" + this.email + ", zip=" + this.zip
+                + ", phone=" + this.phoneNumber + "]";
+    }
 }
